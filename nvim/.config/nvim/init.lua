541696e69d6f281c0395acd962847591f3465943
Daniel Gomez <47473031+HintOfSalt@users.noreply.github.com>
2025-04-03 06:07:22 -0500

    moved to native nvim v11.0 lsp config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,7 +12,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         vim.fn.getchar()
         os.exit(1)
     end
-end
+end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 
@@ -234,62 +234,6 @@ require("lazy").setup({
                 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
             end,
         },
-        {                       -- Useful plugin to show you pending keybinds.
-            'folke/which-key.nvim',
-            event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-            opts = {
-                -- delay between pressing a key and opening which-key (milliseconds)
-                -- this setting is independent of vim.opt.timeoutlen
-                delay = 0,
-                icons = {
-                    -- set icon mappings to true if you have a Nerd Font
-                    mappings = vim.g.have_nerd_font,
-                    -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-                    -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-                    keys = vim.g.have_nerd_font and {} or {
-                        Up = '<Up> ',
-                        Down = '<Down> ',
-                        Left = '<Left> ',
-                        Right = '<Right> ',
-                        C = '<C-…> ',
-                        M = '<M-…> ',
-                        D = '<D-…> ',
-                        S = '<S-…> ',
-                        CR = '<CR> ',
-                        Esc = '<Esc> ',
-                        ScrollWheelDown = '<ScrollWheelDown> ',
-                        ScrollWheelUp = '<ScrollWheelUp> ',
-                        NL = '<NL> ',
-                        BS = '<BS> ',
-                        Space = '<Space> ',
-                        Tab = '<Tab> ',
-                        F1 = '<F1>',
-                        F2 = '<F2>',
-                        F3 = '<F3>',
-                        F4 = '<F4>',
-                        F5 = '<F5>',
-                        F6 = '<F6>',
-                        F7 = '<F7>',
-                        F8 = '<F8>',
-                        F9 = '<F9>',
-                        F10 = '<F10>',
-                        F11 = '<F11>',
-                        F12 = '<F12>',
-                    },
-                },
-
-                -- Document existing key chains
-                spec = {
-                    { '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
-                    { '<leader>d', group = '[D]ocument' },
-                    { '<leader>r', group = '[R]ename' },
-                    { '<leader>s', group = '[S]earch' },
-                    { '<leader>w', group = '[W]orkspace' },
-                    { '<leader>t', group = '[T]oggle' },
-                    { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-                },
-            },
-        },
 
         -- autocompletion
         {
@@ -364,175 +308,11 @@ require("lazy").setup({
 
         -- lsp
         {
-            "neovim/nvim-lspconfig",
+            "williamboman/mason.nvim",
             dependencies = {
-                { "williamboman/mason.nvim", opts = {} },
-                "williamboman/mason-lspconfig.nvim",
                 "WhoIsSethDaniel/mason-tool-installer.nvim",
-                { "j-hui/fidget.nvim",       opts = {} },
-                "hrsh7th/cmp-nvim-lsp",
             },
-            config = function()
-                vim.api.nvim_create_autocmd('LspAttach', {
-                    group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-                    callback = function(event)
-                        local map = function(keys, func, desc, mode)
-                            mode = mode or 'n'
-                            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-                        end
-
-                        -- Jump to the definition of the word under your cursor.
-                        --  This is where a variable was first declared, or where a function is defined, etc.
-                        --  To jump back, press <C-t>.
-                        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-                        -- Find references for the word under your cursor.
-                        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-                        -- Jump to the implementation of the word under your cursor.
-                        --  Useful when your language has ways of declaring types without an actual implementation.
-                        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-                        -- Jump to the type of the word under your cursor.
-                        --  Useful when you're not sure what type a variable is and you want to see
-                        --  the definition of its *type*, not where it was *defined*.
-                        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
-                        -- Fuzzy find all the symbols in your current document.
-                        --  Symbols are things like variables, functions, types, etc.
-                        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-
-                        -- Fuzzy find all the symbols in your current workspace.
-                        --  Similar to document symbols, except searches over your entire project.
-                        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
-                            '[W]orkspace [S]ymbols')
-
-                        -- Rename the variable under your cursor.
-                        --  Most Language Servers support renaming across files, etc.
-                        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-
-                        -- Execute a code action, usually your cursor needs to be on top of an error
-                        -- or a suggestion from your LSP for this to activate.
-                        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-
-                        -- WARN: This is not Goto Definition, this is Goto Declaration.
-                        --  For example, in C this would take you to the header.
-                        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-                        -- The following two autocommands are used to highlight references of the
-                        -- word under your cursor when your cursor rests there for a little while.
-                        --    See `:help CursorHold` for information about when this is executed
-                        --
-                        -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                        local client = vim.lsp.get_client_by_id(event.data.client_id)
-                        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-                            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight',
-                                { clear = false })
-                            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-                                buffer = event.buf,
-                                group = highlight_augroup,
-                                callback = vim.lsp.buf.document_highlight,
-                            })
-
-                            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-                                buffer = event.buf,
-                                group = highlight_augroup,
-                                callback = vim.lsp.buf.clear_references,
-                            })
-
-                            vim.api.nvim_create_autocmd('LspDetach', {
-                                group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-                                callback = function(event2)
-                                    vim.lsp.buf.clear_references()
-                                    vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-                                end,
-                            })
-                        end
-
-                        -- The following code creates a keymap to toggle inlay hints in your
-                        -- code, if the language server you are using supports them
-                        --
-                        -- This may be unwanted, since they displace some of your code
-                        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-                            map('<leader>th', function()
-                                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-                            end, '[T]oggle Inlay [H]ints')
-                        end
-                    end,
-                })
-
-                local capabilities = vim.lsp.protocol.make_client_capabilities()
-                capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-                local servers = {
-                    -- clangd = {},
-                    gopls = {},
-                    zls = {},
-                    --pyright = {},
-                    --bashls = {},
-                    --jsonls = {},
-                    --ts_ls = {},
-                    lua_ls = {
-                        settings = {
-                            Lua = {
-                                completion = {
-                                    callSnippet = 'Replace',
-                                },
-                            },
-                        },
-                    },
-                }
-
-                -- You can add other tools here that you want Mason to install
-                -- for you, so that they are available from within Neovim.
-                local ensure_installed = vim.tbl_keys(servers or {})
-                vim.list_extend(ensure_installed, {
-                    "stylua", -- Used to format Lua code
-                })
-                require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-                require('mason-lspconfig').setup {
-                    handlers = {
-                        function(server_name)
-                            local server = servers[server_name] or {}
-                            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities,
-                                server.capabilities or {})
-                            require('lspconfig')[server_name].setup(server)
-                        end,
-                    },
-                    ensure_installed = {},
-                    automatic_installation = false,
-                }
-            end,
-        },
-        {
-            "MunifTanjim/prettier.nvim",
-            opts = {
-                bin = "prettierd",
-                filetypes = {
-                    "css",
-                    "javascript",
-                    "javascriptreact",
-                    "typescript",
-                    "typescriptreact",
-                    "json",
-                    "scss",
-                    "less",
-                    "markdown",
-                    "yaml",
-                    "html",
-                },
-            },
-        },
-        {
-            'folke/lazydev.nvim',
-            ft = 'lua',
-            opts = {
-                library = {
-                    -- Load luvit types when the `vim.uv` word is found
-                    { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-                },
-            },
+            opts = {}
         },
 
         -- telescope
@@ -774,6 +554,52 @@ require("lazy").setup({
     -- automatically check for plugin updates
     checker = { enabled = true },
 })
+
+------------------
+-- lsp config --
+------------------
+vim.lsp.config("luals", {
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    root_markers = { ".luarc.json", ".luarc.jsonc", ".stylua.toml", "stylua.toml", ".git" },
+    on_init = function(client)
+        local path = vim.tbl_get(client, "workspace_folders", 1, "name")
+        if not path then
+            return
+        end
+        -- override the lua-language-server settings for Neovim config
+        client.settings = vim.tbl_deep_extend('force', client.settings, {
+            Lua = {
+                runtime = {
+                    version = 'LuaJIT'
+                },
+                -- Make the server aware of Neovim runtime files
+                workspace = {
+                    checkThirdParty = false,
+                    library = {
+                        vim.env.VIMRUNTIME
+                        -- "${3rd}/luv/library"
+                    }
+                }
+            }
+        })
+    end
+})
+vim.lsp.enable("luals")
+
+vim.lsp.config("gopls", {
+    cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    root_markers = { "go.mod", "go.work", ".git" }
+})
+vim.lsp.enable("gopls")
+
+vim.lsp.config("zls", {
+    cmd = { "zls" },
+    filetypes = { "zig", "zir" },
+    root_markers = { "zls.json", "build.zig", '.git' }
+})
+vim.lsp.enable('zls')
 
 ------------------
 -- autocommands --
