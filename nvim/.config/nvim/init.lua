27046377511c10aae05ec92dafe73b31c58a54da
Daniel Gomez <47473031+HintOfSalt@users.noreply.github.com>
2025-04-14 23:36:05 -0500

    reorganized plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -181,42 +181,6 @@ require("lazy").setup({
             },
         },
         {
-            "folke/trouble.nvim",
-            opts = {},
-            keys = {
-                {
-                    "<leader>xx",
-                    "<cmd>Trouble diagnostics toggle<cr>",
-                    desc = "Diagnostics (Trouble)",
-                },
-                {
-                    "<leader>xX",
-                    "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-                    desc = "Buffer Diagnostics (Trouble)",
-                },
-                {
-                    "<leader>cs",
-                    "<cmd>Trouble symbols toggle focus=false<cr>",
-                    desc = "Symbols (Trouble)",
-                },
-                {
-                    "<leader>cl",
-                    "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-                    desc = "LSP Definitions / references / ... (Trouble)",
-                },
-                {
-                    "<leader>xL",
-                    "<cmd>Trouble loclist toggle<cr>",
-                    desc = "Location List (Trouble)",
-                },
-                {
-                    "<leader>xQ",
-                    "<cmd>Trouble qflist toggle<cr>",
-                    desc = "Quickfix List (Trouble)",
-                },
-            },
-        },
-        {
             "stevearc/oil.nvim",
             config = function()
                 require("oil").setup({
@@ -442,6 +406,42 @@ require("lazy").setup({
         { "sindrets/diffview.nvim", lazy = true },
 
         -- debugging
+        {
+            "folke/trouble.nvim",
+            opts = {},
+            keys = {
+                {
+                    "<leader>xx",
+                    "<cmd>Trouble diagnostics toggle<cr>",
+                    desc = "Diagnostics (Trouble)",
+                },
+                {
+                    "<leader>xX",
+                    "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                    desc = "Buffer Diagnostics (Trouble)",
+                },
+                {
+                    "<leader>cs",
+                    "<cmd>Trouble symbols toggle focus=false<cr>",
+                    desc = "Symbols (Trouble)",
+                },
+                {
+                    "<leader>cl",
+                    "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                    desc = "LSP Definitions / references / ... (Trouble)",
+                },
+                {
+                    "<leader>xL",
+                    "<cmd>Trouble loclist toggle<cr>",
+                    desc = "Location List (Trouble)",
+                },
+                {
+                    "<leader>xQ",
+                    "<cmd>Trouble qflist toggle<cr>",
+                    desc = "Quickfix List (Trouble)",
+                },
+            },
+        },
         {
             "mfussenegger/nvim-dap",
             dependencies = {
