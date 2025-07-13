eb977b22faba227757263aa2d82dc6ff00fb7e70
Daniel Gomez <47473031+HintOfSalt@users.noreply.github.com>
2025-07-13 09:31:45 -0500

    added nvim html lsp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -576,6 +576,20 @@ vim.lsp.config("zls", {
 })
 vim.lsp.enable('zls')
 
+vim.lsp.config("html", {
+    cmd = { "vscode-html-language-server", "--stdio" },
+    filetypes = { "html", "templ" },
+    root_markers = { "package.json", '.git' },
+    single_file_support = true,
+    settings = {},
+    init_options = {
+        provideFormatter = true,
+        embeddedLanguages = { css = true, javascript = true },
+        configurationSection = { "html", "css", "javascript" },
+    },
+})
+vim.lsp.enable("html")
+
 ------------------
 -- autocommands --
 ------------------
