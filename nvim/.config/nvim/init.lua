e1712e70d45eefbb730987143d1ae67bfe65472d
Daniel Gomez <47473031+HintOfSalt@users.noreply.github.com>
2025-07-13 09:42:13 -0500

    added nvim typescript lsp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -590,6 +590,22 @@ vim.lsp.config("html", {
 })
 vim.lsp.enable("html")
 
+vim.lsp.config("ts_ls", {
+    cmd = { "typescript-language-server", "--stdio" },
+    filetypes = {
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+    },
+    root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+    single_file_support = true,
+    init_options = { hostInfo = "neovim" },
+})
+vim.lsp.enable("ts_ls")
+
 ------------------
 -- autocommands --
 ------------------
